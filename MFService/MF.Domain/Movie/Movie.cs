08e582782a0604953a159454e509e267c760352a
Chamara Dodandeniya <92chamara@gmail.com>
2021-04-12 11:30:40 +0530

    Added movie entity

---
+++ MFService/MF.Domain/Movie/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MF.Domain.Movie
+{
+    public class Movie
+    {
+        public string ImdbID { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Poster { get; set; }
+        public string Plot { get; set; }
+
+        public virtual ICollection<Rating> Ratings { get; set; }
+    }
+}
---
+++ MFService/MF.Domain/Movie/Rating.cs
@@ -0,0 +1,10 @@
+﻿namespace MF.Domain.Movie
+{
+    public class Rating
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+
+        public virtual Movie Movie { get; set; }
+    }
+}
