dd85241775ba09bd9bed1cfdceb856ae4da09511
Chamara Dodandeniya <92chamara@gmail.com>
2021-04-12 12:52:03 +0530

    Added MovieController

---
+++ MFService/MFService/Movies/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MF.Application.Movies.Queries.SearchListOfMovies;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MFService.Movies
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMediator bus;
+
+        public MovieController(IMediator bus)
+        {
+            this.bus = bus;
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public async Task<IActionResult> SearchMovieList([FromQuery] SearchListOfMoviesQuery query)
+        {
+            return Ok(await bus.Send(query));
+        }
+
+    }
+}
