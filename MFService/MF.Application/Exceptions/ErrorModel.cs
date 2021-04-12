76abc87683a15c8f4fcc5b9e76afd9e91cc5a661
Chamara Dodandeniya <92chamara@gmail.com>
2021-04-12 11:41:48 +0530

    Added CustomValidationException class and ErrorModel class for custom error handling

---
+++ MFService/MF.Application/Exceptions/ErrorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MF.Application.Exceptions
+{
+    public class ErrorModel
+    {
+        public ErrorModel(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
